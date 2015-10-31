eff4d7bbe32a60036da16fb06a2e0efdf37428d4
Lisa Vogt <lisavogt.sf@gmail.com>
2015-10-31 08:44:37 -0700

    incremental updates

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Javascript for an implementation of Battleship
+// Javascript including jQuery for an implementation of Battleship
+$(document).ready(function () {
+    console.log("an implementation of Battleship");
+
+    function init (){
+        console.log("initializing a new game state");
+        console.log("clearing out stored game data");
+        console.log("setting up new game boards for yourself and your rival");
+    }
+
+    function reset () {
+        console.log("resetting game, taking us out of an active game state");
+        console.log("ready for user to start a game");
+    }
+
+});
